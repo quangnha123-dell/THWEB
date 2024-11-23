dea954128942597cebe00d20a0c4042dedf6c9ae
ADMIN\Admin <quangnha29082004@gmail.com>
2024-11-23 21:15:22 +0700

    Project

---
+++ BTHWEB/TH/TH/Areas/Adim/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TH.Areas.Adim.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
