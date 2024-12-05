9c9824ff2ef36f93dd9df857f1a6852759f09793
ADMIN\Admin <quangnha29082004@gmail.com>
2024-12-05 22:15:26 +0700

    PROJECTTH4

---
+++ TH3/TH3/Models/TbOrderStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TH3.Models;
+
+public partial class TbOrderStatus
+{
+    public int OrderStatusId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+}
---
+++ TH3/TH3/Models/TbRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TH3.Models;
+
+public partial class TbRole
+{
+    public int RoleId { get; set; }
+
+    public string? RoleName { get; set; }
+
+    public string? Description { get; set; }
+}
