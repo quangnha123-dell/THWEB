04022ead3c27b4c68c2448284272257f6d8cdc28
ADMIN\Admin <quangnha29082004@gmail.com>
2024-12-08 21:16:02 +0700

    bla bla

--- BTHWEB/BTHWEB/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BTHWEB.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
